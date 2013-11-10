385b7e5ae48d85f62d1cb4bdc925ef5279216499
Alex Cooper <sashok.san@gmail.com>
2013-11-11 02:29:24 +0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uumalu::Application.config.secret_token = '8754f76c9260f67de1181f51bcd121256f65c82c6e340e56eabc4687930520ce7c1dc0bd570174c18433a51074f04d56d17b168dce28619d3e2e33a12048e7d1'
